176a810fdca2d784efc351828954e9a80fdf245f
Bishal Regmi <regmibishal1@gmail.com>
2026-03-02 22:22:05 -0500

    Fix Security: Add CORSMiddleware and remove payload logging vulnerability

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.endpoints import hello, health
 from src.dependency import has_access
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200", "https://regmibishal1.github.io"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # routes
 PROTECTED = [Depends(has_access)]
